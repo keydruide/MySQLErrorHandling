0504fae05f61619b859c428d654fdc895de4a565
J0hNs0N-Github <38944943+J0hNs0N-Github@users.noreply.github.com>
2018-05-08 09:31:24 +0200

    Add files via upload

--- scripts/MySqlErrorHandling.js
+++ scripts/MySqlErrorHandling.js
@@ -1,9 +1,10 @@
 $('document').ready(function(){
 
-    $('head').append('<link rel="stylesheet" href="https://github.com/keydruide/MySQLErrorHandling/blob/master/css/bootstrap.css" type="text/css" />');
-    $('head').append('<link rel="stylesheet" href="https://github.com/keydruide/MySQLErrorHandling/blob/master/css/bootstrap.min.css" type="text/css" />');
-    $('head').append('<link rel="stylesheet" href="https://github.com/keydruide/MySQLErrorHandling/blob/master/css/bootstrap-theme.css" type="text/css" />');
-    $('head').append('<link rel="stylesheet" href="https://github.com/keydruide/MySQLErrorHandling/blob/master/css/bootstrap-theme.min.css" type="text/css" />');
+    $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">');
+    $('head').append('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">');
+    $('head').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>');
+    $('head').append('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>');
+    /*$('head').append('<link rel="stylesheet" href="https://github.com/keydruide/MySQLErrorHandling/blob/master/css/bootstrap-theme.css" type="text/css" />');*/
 
     $("body").click(function(){
 
@@ -14,7 +15,7 @@ $('document').ready(function(){
             $(".error").attr("data-content", getException());
             $(".error").attr("title", "MySql Fehler");
             */
-            $('.error').popover({title: "Header", content: "Blabla", placement: "top"});
+            $('.error').popover({title: "Header", content: "Blabla", placement: "bottom"});
         }
     });
 });
